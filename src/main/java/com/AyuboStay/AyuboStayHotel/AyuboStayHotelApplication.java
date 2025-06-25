c6140689f94ec60e333931705b6a68ec29ceb061
WICRAMASINGHA W.L.C. <lakshithaw900@gmail.com>
2025-06-25 18:05:59 +0530

    uploading projet files

---
+++ src/main/java/com/AyuboStay/AyuboStayHotel/AyuboStayHotelApplication.java
@@ -0,0 +1,13 @@
+package com.AyuboStay.AyuboStayHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyuboStayHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyuboStayHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AyuboStay/AyuboStayHotel/AyuboStayHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AyuboStay.AyuboStayHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuboStayHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
